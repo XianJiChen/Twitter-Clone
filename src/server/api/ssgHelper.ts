c5577d664a3a95c143625c0dc03fdad0f9588e48
XianJiChen <xianjichen880813@gmail.com>
2023-07-14 15:54:59 +0800

    make up the uncommited content in ssgHelper.ts

--- src/server/api/ssgHelper.ts
+++ src/server/api/ssgHelper.ts
@@ -7,7 +7,7 @@ export function ssgHelper() {
     return createServerSideHelpers({
         router: appRouter,
         //cause we're doing static site generation so we don't have any user information
-        ctx: createInnerTRPCContext({ session : null}),
+        ctx: createInnerTRPCContext({ session : null, revalidateSSG: null }),
          transformer: superjson
     });
 }
