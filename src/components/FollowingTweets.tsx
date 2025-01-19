72b53a86f0de7e1b9c4de50be736f13cd72bbca1
XianJiChen <xianjichen880813@gmail.com>
2025-01-18 21:21:16 -0600

    update FollowingTweets.tsx

--- src/components/FollowingTweets.tsx
+++ src/components/FollowingTweets.tsx
@@ -1,19 +1,19 @@
-import { api } from "~/utils/api"
+import { api } from "~/utils/api";
 import { InfiniteTweetList } from "./InfiniteTweetList";
 
-export function FollowingTweets () {
-    const tweets = api.tweet.infiniteFeed.useInfiniteQuery(
-        { onlyFollowing: true},
-        { getNextPageParam: (lastPage) => lastPage.nextCursor }
-      );
-    
-      return (
-        <InfiniteTweetList
-          tweets={tweets.data?.pages.flatMap((page) => page.tweets)}
-          isError={tweets.isError}
-          isLoading={tweets.isLoading}
-          hasMore={tweets.hasNextPage==true}
-          fetchNewTweets={tweets.fetchNextPage}
-        />
-      );
+export function FollowingTweets() {
+  const tweets = api.tweet.infiniteFeed.useInfiniteQuery(
+    { onlyFollowing: true },
+    { getNextPageParam: (lastPage) => lastPage.nextCursor }
+  );
+
+  return (
+    <InfiniteTweetList
+      tweets={tweets.data?.pages.flatMap((page) => page.tweets)}
+      isError={tweets.isError}
+      isLoading={tweets.isLoading}
+      hasMore={tweets.hasNextPage == true}
+      fetchNewTweets={tweets.fetchNextPage}
+    />
+  );
 }
